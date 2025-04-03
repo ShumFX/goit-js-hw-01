2bd1065b1ba1ea47fc2803b061ca1be89c9ba915
ShumFX <alfadnepr@gmail.com>
2025-04-03 14:23:18 +0300

    finish

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const contentAll = parseFloat(content);
+  const paddingAll = parseFloat(padding) * 2;
+  const borderAll = parseFloat(border) * 2;
+  return contentAll + paddingAll + borderAll;
+}
+
+//console.log(getElementWidth("50px", "8px", "4px"));
+//console.log(getElementWidth("60px", "12px", "8.5px"));
+//console.log(getElementWidth("200px", "0px", "0px"));
